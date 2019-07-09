0ff71ff3012a40c1c8d2aadbe770d0a0e4687a00
KarinaVanyuk <mmf.vanyukki@gmail.com>
2019-07-09 20:53:28 +0300

    updated swagger

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const port = 3000;
+const swagger = require("./routes/swagger.js");
+
+const router = express.Router().use("/", swagger);
+
+const app = express()
+.use("/docs", swagger);
+
+app.listen(port, (err) => {
+  if (err) {
+      return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
