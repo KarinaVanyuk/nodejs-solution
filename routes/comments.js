1c9c9acdd00388899a0455f4bca16868ec5748ea
KarinaVanyuk <mmf.vanyukki@gmail.com>
2019-08-02 12:09:05 +0300

    revert

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const commentsController = require('../controllers/comments.js');
 
 router.get("/:postId/comments", commentsController.getComments);
