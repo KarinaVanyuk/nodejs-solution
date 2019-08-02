47d7f88914c78d6696efc16456ddf30465b305d5
Karyna Vaniuk <mmf.vanyukki@gmail.com>
2019-08-02 12:12:25 +0300

    Revert "WIP: Hw 2"

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 const express = require("express");
-const bodyParser = require('body-parser');
 const port = 3000;
 const swagger = require("./routes/swagger.js");
-const posts = require("./routes/posts.js");
-const users = require("./routes/users.js");
-const comments = require("./routes/comments.js");
+
+const router = express.Router().use("/", swagger);
 
 const app = express()
-app.use(bodyParser.json());
-
-app.use("/docs", swagger)
-   .use('/posts', posts)
-   .use('/user', users)
-   .use('/post', comments)
+.use("/docs", swagger)
 
 app.listen(port, (err) => {
   if (err) {
